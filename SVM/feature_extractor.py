a9fc7aea87efd8d53427783c2f72d524b4b4a2ef
apolitowicz <politowicz205@gmail.com>
2017-11-05 17:45:58 -0600

    setup framework for feature_extractor fxns

--- SVM/feature_extractor.py
+++ SVM/feature_extractor.py
@@ -5,3 +5,35 @@ Created on Thu Nov  2 19:43:35 2017
 @author: tushar
 """
 
+import cv2
+import numpy as np
+
+def extractYellowness(image):
+    norm_img = image
+    #cv2.normalize(image, norm_img, alpha=0, beta=20, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+
+    #hist = cv2.calcHist([norm_img], [0], None, [2], [0,20])
+    #back_project = cv2.calcBackProject([norm_img], [0], hist, [0,20], 1)
+
+    #filtered_img = cv2.applyColorMap(norm_img, cv2.COLORMAP_RAINBOW)
+
+    #DEBUG: display image
+    cv2.imshow('filtered image', norm_img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    return 0
+
+def extractCircles(image):
+    circles = cv2.HoughCircles(image, cv2.HOUGH_GRADIENT, 1, 20, param1=50, param2=30, minRadius=350, maxRadius=500)
+    circles = np.uint16(np.around(circles))
+
+    #DEBUG: draw circles
+    #for i in circles[0,:]:
+    #    cv2.circle(image, (i[0], i[1]), i[2], (0, 255, 0), 2)
+    #    cv2.circle(image, (i[0], i[1]), 2, (0, 0, 255), 3)
+    #cv2.imshow('detected circles', image)
+    #cv2.waitKey(0)
+    #cv2.destroyAllWindows()
+
+    return circles
--- SVM/svm_pof.py
+++ SVM/svm_pof.py
@@ -7,10 +7,15 @@ Created on Tue Oct 31 19:19:33 2017
 import cv2
 import numpy as np
 from sklearn import svm
-clf = svm.SVC(gamma=0.001, C=100.)
+
+import feature_extractor
+
+#clf = svm.SVC(gamma=0.001, C=100.)
 
 img = cv2.imread("tennis_ball.jpg")
 gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+blur_img = cv2.medianBlur(img, 45)
+blur_gray_img = cv2.cvtColor(blur_img, cv2.COLOR_BGR2GRAY)
 
 #height, width = img.shape[:2]
 
@@ -18,5 +23,13 @@ d_img = np.reshape(gray_img, (1,np.product(gray_img.shape)))
 
 height, width = d_img.shape[:2]
 
-print(str(height) + "x" + str(width)) 
+# get features
+feature_extractor.extractYellowness(gray_img)
+#feature_extractor.extractCircles(blur_gray_img)
 
+#DEBUG
+print(str(height) + "x" + str(width))
+
+#cv2.imshow('blurred gray image', blur_gray_img)
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
