4d7afd457178a413ea5cbdb1ddc9e96e1e502c03
David Fei <dfei@wisc.edu>
2017-11-28 20:21:25 -0600

    Blah
    TODO: Run the training script in the tensorflow example code
    Then write some more tensorflow code to use that data to classify tennis
    balls.

--- CNN/cnn_pof.py
+++ CNN/cnn_pof.py
@@ -9,4 +9,7 @@ import tensorflow as tf
 hello = tf.constant('Hello, TensorFlow!')
 sess = tf.Session()
 print(sess.run(hello))
-
+node1 = tf.constant(3.0, dtype=tf.float32)
+node2 = tf.constant(4.0)
+print(node1, node2)
+print(sess.run([node1, node2]))
