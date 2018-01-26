be5e00fc09d4594983a94c5c5dff4b016ad268d1
davidfei222 <dfei@wisc.edu>
2018-01-25 20:53:03 -0600

    Get the required libraries to work
    The tensorflow jupyter tutorial requires all of these libraries in order
    to work through it.  We need to get them to work (I've been running into
    errors).

--- CNN/cnn_pof.py
+++ CNN/cnn_pof.py
@@ -2,9 +2,20 @@
 """
 Created on Thu Oct 19 17:06:01 2017
 
-@author: tushar
+@author: david
 """
+import numpy as np
+import os
+import six.moves.urllib as urllib
+import sys
+import tarfile
 import tensorflow as tf
+import zipfile
+
+from collections import defaultdict
+from io import StringIO
+from matplotlib import pyplot as plt
+from PIL import Image
 
 hello = tf.constant('Hello, TensorFlow!')
 sess = tf.Session()
