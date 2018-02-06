0c747e1713fe00ef9b6eea63fb720adfc5bd8cca
davidfei222 <dfei@wisc.edu>
2018-02-05 19:07:45 -0600

    Finished setting up classifier code
    Once we have a model and test images, the corresponding variables for
    those will need to be filled in to make this work.

--- CNN/cnn_pof.py
+++ CNN/cnn_pof.py
@@ -70,13 +70,48 @@ for i in range(1, NUM_IMGS):
 # Image size specification
 IMG_SIZE = (10, 10)
 
+# Classify the images
+with detection_graph.as_default():
+    with tf.Session(graph=detection_graph) as sess:
+        # Definite input and output Tensors for detection_graph
+        image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+        # Each box represents a part of the image where a particular object was detected.
+        detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+        # Each score represent how level of confidence for each of the objects.
+        # Score is shown on the result image, together with the class label.
+        detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
+        detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
+        num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+        for image_path in TEST_IMAGE_PATHS:
+            image = Image.open(image_path)
+            # the array based representation of the image will be used later in order to prepare the
+            # result image with boxes and labels on it.
+            image_np = load_image_into_numpy_array(image)
+            # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+            image_np_expanded = np.expand_dims(image_np, axis=0)
+            # Actual detection.
+            (boxes, scores, classes, num) = sess.run(
+                [detection_boxes, detection_scores, detection_classes, num_detections],
+                feed_dict={image_tensor: image_np_expanded})
+            # Visualization of the results of a detection.
+            vis_util.visualize_boxes_and_labels_on_image_array(
+                image_np,
+                np.squeeze(boxes),
+                np.squeeze(classes).astype(np.int32),
+                np.squeeze(scores),
+                category_index,
+                use_normalized_coordinates=True,
+                line_thickness=8)
+            plt.figure(figsize=IMAGE_SIZE)
+            plt.imshow(image_np)
+
 # Tensorflow test code
-hello = tf.constant('Hello, TensorFlow!')
-sess = tf.Session()
-print(sess.run(hello))
-node1 = tf.constant(3.0, dtype=tf.float32)
-node2 = tf.constant(4.0)
-print(node1, node2)
-print(sess.run([node1, node2]))
+#hello = tf.constant('Hello, TensorFlow!')
+#sess = tf.Session()
+#print(sess.run(hello))
+#node1 = tf.constant(3.0, dtype=tf.float32)
+#node2 = tf.constant(4.0)
+#print(node1, node2)
+#print(sess.run([node1, node2]))
 
 
