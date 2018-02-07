3dc77f31e83b6e7334a64350ea4374028e9cc5dc
davidfei222 <dfei@wisc.edu>
2018-02-06 20:35:02 -0600

    Began writing trainer
    Still need to sort out installing the necessary libraries

---
+++ CNN/tennis_ball_trainer.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+@author: David Fei
+"""
+import tensorflow as tf
+
+classes = ['tennis ball', 'no tennis ball']
+num_classes = len(classes)
+train_path = 'training_data'
+
+# validation split
+# This value can be adjusted if we want to use more training images for validation.
+validation_size = 0.2
+
+batch_size = 16
+
+# TODO: Implement the dataset class to give it the functionality to read images from the training
+# and test data folders.
+data = dataset.read_train_sets(train_path, img_size, classes, validation_size=validation_size)
